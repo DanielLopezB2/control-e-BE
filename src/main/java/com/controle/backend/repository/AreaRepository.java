cddb8316963a786a24bd854bda467efa0109f800
Daniel Lopez <dalobe56@gmail.com>
2024-07-13 00:43:46 -0500

    Add repositories

---
+++ src/main/java/com/controle/backend/repository/AreaRepository.java
@@ -0,0 +1,8 @@
+package com.controle.backend.repository;
+
+import com.controle.backend.model.Area;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaRepository extends JpaRepository<Area, Long> {
+
+}
---
+++ src/main/java/com/controle/backend/repository/AsignaturaRepository.java
@@ -0,0 +1,7 @@
+package com.controle.backend.repository;
+
+import com.controle.backend.model.Asignatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AsignaturaRepository extends JpaRepository<Asignatura, Long> {
+}
