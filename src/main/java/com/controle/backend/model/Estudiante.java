9c2433d3f840a814a26e9079eaea907c83a7e0e5
Daniel Lopez <dalobe56@gmail.com>
2024-07-13 00:57:59 -0500

    Add Estudiante model

---
+++ src/main/java/com/controle/backend/model/Estudiante.java
@@ -0,0 +1,58 @@
+package com.controle.backend.model;
+
+import com.controle.backend.enums.Estado;
+import com.controle.backend.enums.Genero;
+import com.controle.backend.enums.TipoDocumentoIdentidad;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Estudiante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombres;
+
+    @Column(nullable = false)
+    private String apellidos;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoDocumentoIdentidad tipoDocumentoIdentidad;
+
+    @Column(nullable = false)
+    private Long numeroDocumento;
+
+    @Column(nullable = false)
+    @Email(message = "Debe ingresar un email válido")
+    private String email;
+
+    private Long telefono;
+
+    @Column(nullable = false)
+    private Genero genero;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDate fechaCreacion;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDate fechaUltimaModificacion;
+}
